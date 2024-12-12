21bba8a88f0d6933db67b400fb4e05dde5c876bf
NahuelAlaniz <nahuelalaniz80@gmail.com>
2024-12-12 15:10:38 -0300

    Hello Word

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [],
+  template: `
+     <h1>Default</h1>
+  `,
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-second-angular-app';
+  title = 'default';
 }
