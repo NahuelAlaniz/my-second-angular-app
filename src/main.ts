1a626cda133d999eb632751edcea235a42b6f761
NahuelAlaniz <nahuelalaniz80@gmail.com>
2024-12-13 16:37:16 -0300

    Create Home Component

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { bootstrapApplication, provideProtractorTestingSupport } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent, {providers:[provideProtractorTestingSupport()]})
   .catch((err) => console.error(err));
